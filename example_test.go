92956aba4c3829d1fd71b0916ae8284a96644377
bytemare <{ID}+{username}@users.noreply.github.com>
2020-12-05 19:31:37 +0100

    expose Finalize() for single inputs

--- example_test.go
+++ example_test.go
@@ -8,13 +8,12 @@ package voprf
 
 import (
 	"fmt"
-
-	"github.com/bytemare/cryptotools/encoding"
 )
 
 func ExampleBaseMultiplicative() {
 	input := []byte("input")
-	enc := encoding.JSON
+	info := []byte("some app info")
+	// enc := encoding.JSON
 
 	// Set up a new server. A private key is automatically created if using nil as parameter.
 	server, err := RistrettoSha512.Server(nil)
@@ -23,33 +22,41 @@ func ExampleBaseMultiplicative() {
 	}
 
 	// Set up a new client
-	client := RistrettoSha512.Client()
+	client, err := RistrettoSha512.Client(nil)
+	if err != nil {
+		panic(err)
+	}
 
 	// The client blinds the initial input, and sends this to the server.
-	blinded := client.Start(input)
+	blinded := client.Blind(input)
 
-	// The server evaluates the blinded input, and returns an Evaluation.
+	// The server evaluates the blinded input, and returns an evaluation.
 	// This evaluation also contains proofs if Verifiable mode is requested.
 	evaluation, err := server.Evaluate(blinded)
 	if err != nil {
 		panic(err)
 	}
 
-	// The server encodes the Evaluation, and sends it to the client
-	eval, err := evaluation.Encode(enc)
-	if err != nil {
-		panic(err)
-	}
+	// The server encodes the evaluation, and sends it to the client
+	//Evaluation, err := evaluation.encode(enc)
+	//if err != nil {
+	//	panic(err)
+	//}
 
 	// The client wraps up the protocol execution, by decoding the received evaluation,
 	// reverting the blinding and hashing the protocol transcript.
-	output, err := client.Finish(eval, enc)
+	//unblinded, err := client.unblind(evaluation, nil, nil)
+	//if err != nil {
+	//	panic(err)
+	//}
+
+	output, err := client.Finalize(evaluation, info)
 	if err != nil {
 		panic(err)
 	}
 
 	// This is not part of the protocol, but allows output verification
-	if server.VerifyFinalize(input, output, client.info) {
+	if server.VerifyFinalize(input, output, info) {
 		fmt.Printf("Success! The OPRF works as intended.")
 	} else {
 		fmt.Printf("Something went wrong.")
@@ -57,75 +64,79 @@ func ExampleBaseMultiplicative() {
 	// Output: Success! The OPRF works as intended.
 }
 
-func ExampleBaseAdditive() {
-	input := []byte("input")
-	enc := encoding.JSON
-
-	server, err := RistrettoSha512.Server(nil)
-	if err != nil {
-		panic(err)
-	}
-
-	// Get the encoded public key, to be send to clients through another channel
-	serverPubKey := server.PublicKey()
-
-	// The client preprocesses some values given the server's public key,
-	// and should store it and retrieve it when needed.
-	preprocessed, err := RistrettoSha512.Preprocess(serverPubKey)
-	if err != nil {
-		panic(err)
-	}
-
-	// Encode the preprocessed values for storage, and store it.
-	encoded, err := preprocessed.Encode(enc)
-	if err != nil {
-		panic(err)
-	}
-
-	// Upon retrieval, decode/restore it.
-	decoded, err := RistrettoSha512.DecodePreprocessedBlind(encoded, enc)
-	if err != nil {
-		panic(err)
-	}
-
-	// Instantiate a new client with the preprocessed values
-	client := RistrettoSha512.ClientAdditive(decoded)
-
-	// The client blinds the initial input, and sends this to the server.
-	blinded := client.Start(input)
-
-	// The server evaluates the blinded input, and returns an Evaluation.
-	// This evaluation also contains proofs if Verifiable mode is requested.
-	evaluation, err := server.Evaluate(blinded)
-	if err != nil {
-		panic(err)
-	}
-
-	// The server encodes the Evaluation, and sends it to the client
-	eval, err := evaluation.Encode(enc)
-	if err != nil {
-		panic(err)
-	}
-
-	// The client wraps up the protocol execution, by decoding the received evaluation,
-	// reverting the blinding and hashing the protocol transcript.
-	output, err := client.Finish(eval, enc)
-	if err != nil {
-		panic(err)
-	}
-
-	// This is not part of the protocol, but allows output verification
-	if server.VerifyFinalize(input, output, client.info) {
-		fmt.Printf("Success! The OPRF works as intended.")
-	} else {
-		fmt.Printf("Something went wrong.")
-	}
-	// Output: Success! The OPRF works as intended.
-}
+//func ExampleBaseAdditive() {
+//	input := []byte("input")
+//	enc := encoding.JSON
+//
+//	server, err := RistrettoSha512.Server(nil)
+//	if err != nil {
+//		panic(err)
+//	}
+//
+//	// Get the encoded public key, to be send to clients through another channel
+//	serverPubKey := server.PublicKey()
+//
+//	// The client preprocesses some values given the server's public key,
+//	// and should store it and retrieve it when needed.
+//	preprocessed, err := RistrettoSha512.Preprocess(serverPubKey)
+//	if err != nil {
+//		panic(err)
+//	}
+//
+//	// encode the preprocessed values for storage, and store it.
+//	//encoded, err := preprocessed.encode(enc)
+//	//if err != nil {
+//	//	panic(err)
+//	//}
+////
+//	//// Upon retrieval, decode/restore it.
+//	//decoded, err := RistrettoSha512.DecodePreprocessedBlind(encoded, enc)
+//	//if err != nil {
+//	//	panic(err)
+//	//}
+//
+//	// Instantiate a new client with the preprocessed values
+//	client, err := RistrettoSha512.ClientAdditive(serverPubKey, preprocessed)
+//	if err != nil {
+//		panic(err)
+//	}
+//
+//	// The client blinds the initial input, and sends this to the server.
+//	blinded := client.blind(input)
+//
+//	// The server evaluates the blinded input, and returns an evaluation.
+//	// This evaluation also contains proofs if Verifiable mode is requested.
+//	evaluation, err := server.Evaluate(blinded)
+//	if err != nil {
+//		panic(err)
+//	}
+//
+//	// The server encodes the evaluation, and sends it to the client
+//	Evaluation, err := evaluation.encode(enc)
+//	if err != nil {
+//		panic(err)
+//	}
+//
+//	// The client wraps up the protocol execution, by decoding the received evaluation,
+//	// reverting the blinding and hashing the protocol transcript.
+//	output, err := client.Finish(Evaluation, enc)
+//	if err != nil {
+//		panic(err)
+//	}
+//
+//	// This is not part of the protocol, but allows output verification
+//	if server.VerifyFinalize(input, output, client.info) {
+//		fmt.Printf("Success! The OPRF works as intended.")
+//	} else {
+//		fmt.Printf("Something went wrong.")
+//	}
+//	// Output: Success! The OPRF works as intended.
+//}
 
 func ExampleVerifiableMultiplicative() {
 	input := []byte("input")
-	enc := encoding.JSON
+	info := []byte("some app info")
+	// enc := encoding.JSON
 
 	server, err := RistrettoSha512.VerifiableServer(nil)
 	if err != nil {
@@ -136,36 +147,43 @@ func ExampleVerifiableMultiplicative() {
 	pubKey := server.PublicKey()
 
 	// Instantiate a new client that will verify the server's responses with the server's public key
-	client, err := RistrettoSha512.VerifiableClient(pubKey)
+	client, err := RistrettoSha512.Client(pubKey)
 	if err != nil {
 		panic(err)
 	}
 
 	// The client blinds the initial input, and sends this to the server.
-	blinded := client.Start(input)
+	blinded := client.Blind(input)
 
-	// The server evaluates the blinded input, and returns an Evaluation.
+	// The server evaluates the blinded input, and returns an evaluation.
 	// This evaluation also contains proofs if Verifiable mode is requested.
 	evaluation, err := server.Evaluate(blinded)
 	if err != nil {
 		panic(err)
 	}
 
-	// The server encodes the Evaluation, and sends it to the client
-	eval, err := evaluation.Encode(enc)
-	if err != nil {
-		panic(err)
-	}
+	// The server encodes the evaluation, and sends it to the client
+	//Evaluation, err := evaluation.encode(enc)
+	//if err != nil {
+	//	panic(err)
+	//}
 
 	// The client wraps up the protocol execution, by decoding the received evaluation,
 	// reverting the blinding and hashing the protocol transcript.
-	output, err := client.Finish(eval, enc)
+	//unblinded, err := client.unblind(evaluation.element[0].Bytes(), evaluation.proofC.Bytes(), evaluation.proofS.Bytes())
+	//if err != nil {
+	//	panic(err)
+	//}
+	//
+	//output := client.finalize(unblinded, info)
+
+	output, err := client.Finalize(evaluation, info)
 	if err != nil {
 		panic(err)
 	}
 
 	// This is not part of the protocol, but allows output verification
-	if server.VerifyFinalize(input, output, client.info) {
+	if server.VerifyFinalize(input, output, info) {
 		fmt.Printf("Success! The OPRF works as intended.")
 	} else {
 		fmt.Printf("Something went wrong.")
@@ -173,71 +191,71 @@ func ExampleVerifiableMultiplicative() {
 	// Output: Success! The OPRF works as intended.
 }
 
-func ExampleVerifiableAdditive() {
-	input := []byte("input")
-	enc := encoding.JSON
-
-	server, err := RistrettoSha512.VerifiableServer(nil)
-	if err != nil {
-		panic(err)
-	}
-
-	// Get the encoded public key, to be send to clients through another channel
-	serverPubKey := server.PublicKey()
-
-	// The client preprocesses some values given the server's public key,
-	// and should store it and retrieve it when needed.
-	preprocessed, err := RistrettoSha512.Preprocess(serverPubKey)
-	if err != nil {
-		panic(err)
-	}
-
-	// Encode the preprocessed values for storage, and store it.
-	encoded, err := preprocessed.Encode(enc)
-	if err != nil {
-		panic(err)
-	}
-
-	// Upon retrieval, decode/restore it.
-	decoded, err := RistrettoSha512.DecodePreprocessedBlind(encoded, enc)
-	if err != nil {
-		panic(err)
-	}
-
-	// Instantiate a new client with the preprocessed values
-	client, err := RistrettoSha512.VerifiableClientAdditive(serverPubKey, decoded)
-	if err != nil {
-		panic(err)
-	}
-
-	// The client blinds the initial input, and sends this to the server.
-	blinded := client.Start(input)
-
-	// The server evaluates the blinded input, and returns an Evaluation.
-	// This evaluation also contains proofs if Verifiable mode is requested.
-	evaluation, err := server.Evaluate(blinded)
-	if err != nil {
-		panic(err)
-	}
-
-	// The server encodes the Evaluation, and sends it to the client
-	eval, err := evaluation.Encode(enc)
-	if err != nil {
-		panic(err)
-	}
-
-	// The client wraps up the protocol execution, by decoding the received evaluation,
-	// reverting the blinding and hashing the protocol transcript.
-	output, err := client.Finish(eval, enc)
-	if err != nil {
-		panic(err)
-	}
-
-	// This is not part of the protocol, but allows output verification
-	if server.VerifyFinalize(input, output, client.info) {
-		fmt.Printf("Success! The OPRF works as intended.")
-	} else {
-		fmt.Printf("Something went wrong.")
-	}
-	// Output: Success! The OPRF works as intended.
-}
+//func ExampleVerifiableAdditive() {
+//	input := []byte("input")
+//	enc := encoding.JSON
+//
+//	server, err := RistrettoSha512.VerifiableServer(nil)
+//	if err != nil {
+//		panic(err)
+//	}
+//
+//	// Get the encoded public key, to be send to clients through another channel
+//	serverPubKey := server.PublicKey()
+//
+//	// The client preprocesses some values given the server's public key,
+//	// and should store it and retrieve it when needed.
+//	preprocessed, err := RistrettoSha512.Preprocess(serverPubKey)
+//	if err != nil {
+//		panic(err)
+//	}
+//
+//	// encode the preprocessed values for storage, and store it.
+//	encoded, err := preprocessed.encode(enc)
+//	if err != nil {
+//		panic(err)
+//	}
+//
+//	// Upon retrieval, decode/restore it.
+//	decoded, err := RistrettoSha512.DecodePreprocessedBlind(encoded, enc)
+//	if err != nil {
+//		panic(err)
+//	}
+//
+//	// Instantiate a new client with the preprocessed values
+//	client, err := RistrettoSha512.VerifiableClientAdditive(serverPubKey, decoded)
+//	if err != nil {
+//		panic(err)
+//	}
+//
+//	// The client blinds the initial input, and sends this to the server.
+//	blinded := client.Start(input)
+//
+//	// The server evaluates the blinded input, and returns an evaluation.
+//	// This evaluation also contains proofs if Verifiable mode is requested.
+//	evaluation, err := server.Evaluate(blinded)
+//	if err != nil {
+//		panic(err)
+//	}
+//
+//	// The server encodes the evaluation, and sends it to the client
+//	Evaluation, err := evaluation.encode(enc)
+//	if err != nil {
+//		panic(err)
+//	}
+//
+//	// The client wraps up the protocol execution, by decoding the received evaluation,
+//	// reverting the blinding and hashing the protocol transcript.
+//	output, err := client.Finish(Evaluation, enc)
+//	if err != nil {
+//		panic(err)
+//	}
+//
+//	// This is not part of the protocol, but allows output verification
+//	if server.VerifyFinalize(input, output, client.info) {
+//		fmt.Printf("Success! The OPRF works as intended.")
+//	} else {
+//		fmt.Printf("Something went wrong.")
+//	}
+//	// Output: Success! The OPRF works as intended.
+//}
