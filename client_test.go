ff8727287cfeb2b2d78bb2cfde9a36fddd656f01
bytemare <{ID}+{username}@users.noreply.github.com>
2021-02-04 04:13:57 +0100

    add tests

---
+++ client_test.go
@@ -0,0 +1,96 @@
+package voprf
+
+import (
+	"bytes"
+	"github.com/bytemare/cryptotools/encoding"
+	"reflect"
+	"testing"
+)
+
+func testExport(t *testing.T, client *Client, export *State) {
+	if export.Ciphersuite != client.id {
+		t.Fatal("ciphersuite does not match")
+	}
+
+	if export.Mode != client.mode {
+		t.Fatal("mode is not correct")
+	}
+
+	if export.Blinding != client.blinding {
+		t.Fatal("blinding is not correct")
+	}
+
+	if !bytes.Equal(export.ServerPublicKey, client.serverPublicKey.Bytes()) {
+		t.Fatal("blind is not correct")
+	}
+
+	for i, b := range client.input {
+		if !bytes.Equal(export.Input[i], b) {
+			t.Fatalf("input %d is not correct", i)
+		}
+	}
+
+	for i, b := range client.blind {
+		if !bytes.Equal(export.Blind[i], b.Bytes()) {
+			t.Fatalf("blind %d is not correct", i)
+		}
+	}
+
+	if client.preprocessedBLind != nil {
+		if !bytes.Equal(export.PreprocessedBlind.BlindedPubKey, client.preprocessedBLind.blindedPubKey.Bytes()) {
+			t.Fatal("blinded pubkey is not correct")
+		}
+
+		if !bytes.Equal(export.PreprocessedBlind.BlindedGenerator, client.preprocessedBLind.blindedGenerator.Bytes()) {
+			t.Fatal("blinded generator is not correct")
+		}
+	}
+}
+
+func TestClient_Export(t *testing.T) {
+	suite := RistrettoSha512
+	input := []byte("input")
+	server, _ := suite.Server(nil)
+	serverPubKey := server.PublicKey()
+	client, _ := suite.Client(serverPubKey)
+	client.Blind(input)
+
+	x := client.Export()
+
+	testExport(t, client, x)
+}
+
+func TestClient_Import(t *testing.T) {
+	suite := RistrettoSha512
+	enc := encoding.JSON
+	input := []byte("input")
+	server, _ := suite.Server(nil)
+	serverPubKey := server.PublicKey()
+	client, _ := suite.Client(serverPubKey)
+	client.Blind(input)
+	export := client.Export()
+
+	testExport(t, client, export)
+
+	encoded, err := enc.Encode(export)
+	if err != nil {
+		panic(err)
+	}
+
+	decoded, err := enc.Decode(encoded, &State{})
+	if err != nil {
+		panic(err)
+	}
+	export2 := decoded.(*State)
+
+	if !reflect.DeepEqual(export, export2) {
+		t.Fatal("Export encoding/decoding failed.")
+	}
+
+	clientCopy, _ := P256Sha256.Client(nil)
+	if err := clientCopy.Import(export2); err != nil {
+		panic(err)
+	}
+
+	testExport(t, clientCopy, export)
+}
--- go.mod
+++ go.mod
@@ -3,8 +3,8 @@ module github.com/bytemare/voprf
 go 1.15
 
 require (
-	github.com/bytemare/cryptotools v0.0.0-20201231013943-c22486d52c20
+	github.com/bytemare/cryptotools v0.0.0-20210204004754-ddabbe01cffa
 	github.com/stretchr/testify v1.6.1
-	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad // indirect
-	golang.org/x/sys v0.0.0-20201231184435-2d18734c6014 // indirect
+	github.com/vmihailenco/msgpack/v5 v5.2.0 // indirect
+	golang.org/x/sys v0.0.0-20210124154548-22da62e12c0c // indirect
 )
